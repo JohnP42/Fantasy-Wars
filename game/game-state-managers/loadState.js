81f54dfddfe82feab3a88d46c5390757504ded8d
John Polhill <JohnPolhill42@gmail.com>
2016-01-08 15:48:27 -0600

    Add map for testing

--- game/game-state-managers/loadState.js
+++ game/game-state-managers/loadState.js
@@ -3,9 +3,10 @@ var loadState = {
 	preload: function() {
 		//TODO: Load assets here
     this.load.image('logo', 'game/assets/logo.jpg');
+    game.load.tilemap("testmap", "game/assets/maps/test_map.json", null, Phaser.Tilemap.TILED_JSON );
 	},
 
 	create: function() {
-		game.state.start("menuState");
+		game.state.start("battleState");
 	}
 }
