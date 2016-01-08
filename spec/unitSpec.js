a4e762f0c4404260f8f0a66aa67ad4bb5b5197ba
Kevin Mark & Max Iniguez <pair+kmark1625.maximillini@devbootcamp.com>
2016-01-08 17:24:09 -0600

    Add specs

--- spec/unitSpec.js
+++ spec/unitSpec.js
@@ -14,13 +14,55 @@ describe("Unit", function() {
     });
   });
 
+  describe("attack", function() {
+    xit("Allows you to attack an enemy unit", function() {
+
+    });
+  });
+
+  describe("getPossibleMoves", function() {
+    xit("Returns all possible moves from a given location", function() {
+
+    });
+  });
+
+  describe("getPossibleAttacks", function() {
+    xit("Returns all possible attacks from a given location", function() {
+
+    });
+  });
+
+  describe("animate", function() {
+    xit("Updates animation state appropriately", function() {
+
+    });
+  });
+
   describe('takeDamage', function() {
     it("Able to take damage from an attack", function() {
       unit.takeDamage(50);
       expect(unit.health).toEqual(50)
     });
-    xit("Unit is destroyed if it takes fatal damage", function() {
+    xit("Die is called if unit takes fatal damage", function() {
       // TODO
+    });
+  });
+
+  describe("getHealthNumber", function() {
+    xit("Translates health percentage into a displayable number 1-10", function() {
+
+    });
+  });
+
+  describe("getAttackDamage", function() {
+    xit("It returns attack damage on a given position", function() {
+
+    });
+  });
+
+  describe("die", function() {
+    xit("Unit is destroyed", function() {
+
     });
   });
 });
