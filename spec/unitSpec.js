51da55d1a324ca5fb91b0ae1121753e8b62439de
Kevin Mark & Max Iniguez <pair+kmark1625.maximillini@devbootcamp.com>
2016-01-08 15:47:45 -0600

    Update paths in SpecRunner.html

--- spec/unitSpec.js
+++ spec/unitSpec.js
@@ -1,14 +1,16 @@
 describe("Unit", function() {
 
   beforeEach(function() {
-    var location = new Location(1,1)
-    var unit = new Unit(location)
+    var position = new Position(1,1);
+    // var unit = new Unit(location);
   });
 
   describe("move", function() {
     it("moves a unit from one location to target location", function() {
-      Unit.move([[1,0], [0,0]];
-      expect(unit.location.coordinates).toBe([0,0]));
+      var unit = new Unit(position);
+      // unit.move([[1,0], [0,0]]);
+      console.log(unit.position)
+      expect(unit.position.coordinates).toEqual([0,0]);
     });
   });
 });
