0c2e577bf1be254e266d2ae72bc874a1d30b5b97
Kevin Mark & Max Iniguez <pair+kmark1625.maximillini@devbootcamp.com>
2016-01-08 15:17:51 -0600

    Troubleshoot spec

--- spec/unitSpec.js
+++ spec/unitSpec.js
@@ -1,12 +1,14 @@
 describe("Unit", function() {
+
   beforeEach(function() {
-    var location =
+    var location = new Location(1,1)
     var unit = new Unit(location)
-  })
+  });
+
   describe("move", function() {
     it("moves a unit from one location to target location", function() {
-      Unit.move([[1,1], [1,0], [0,0]]
-      expect(unit.location.coordinates).toBe([0,0]))
-    })
-  })
-})
+      Unit.move([[1,0], [0,0]];
+      expect(unit.location.coordinates).toBe([0,0]));
+    });
+  });
+});
