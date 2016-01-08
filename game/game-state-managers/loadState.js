601438d3d95a2ca82b6af16b4d91999eb263f49d
John Polhill <JohnPolhill42@gmail.com>
2016-01-08 16:02:58 -0600

    Add working map

--- game/game-state-managers/loadState.js
+++ game/game-state-managers/loadState.js
@@ -4,6 +4,7 @@ var loadState = {
 		//TODO: Load assets here
     this.load.image('logo', 'game/assets/logo.jpg');
     game.load.tilemap("testmap", "game/assets/maps/test_map.json", null, Phaser.Tilemap.TILED_JSON );
+    game.load.image("tilesheet", "game/assets/maps/example_tilesheet_2.png");
 	},
 
 	create: function() {
