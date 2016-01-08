3858f2d3c94246d917e57706b36b5b712067c0be
Kevin Mark & Max Iniguez <pair+kmark1625.maximillini@devbootcamp.com>
2016-01-08 15:58:09 -0600

    Add working move method and spec

--- spec/unitSpec.js
+++ spec/unitSpec.js
@@ -1,16 +1,19 @@
 describe("Unit", function() {
 
   beforeEach(function() {
-    var position = new Position(1,1);
+    // var position = new Position(1,1);
     // var unit = new Unit(location);
   });
 
   describe("move", function() {
     it("moves a unit from one location to target location", function() {
+      var position = new Position(1,1);
+      var position2 = new Position(1,0);
+      var position3 = new Position(0,0);
       var unit = new Unit(position);
-      // unit.move([[1,0], [0,0]]);
-      console.log(unit.position)
-      expect(unit.position.coordinates).toEqual([0,0]);
+      unit.move([position2, position3]);
+      // console.log(unit.position);
+      expect(unit.position.getCoordinates()).toEqual([0,0]);
     });
   });
 });
