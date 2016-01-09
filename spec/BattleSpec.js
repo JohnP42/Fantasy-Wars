1806bec8ebcc3c8592ad5d0b485bdd227d451046
Kevin Mark & Max Iniguez <pair+kmark1625.maximillini@devbootcamp.com>
2016-01-08 18:01:42 -0600

    Add spec shell for Player and Battle

---
+++ spec/BattleSpec.js
@@ -0,0 +1,22 @@
+describe("Battle", function() {
+
+  describe("getUnitAtLocation", function() {
+    xit("Returns the appropriate unit at the given location", function() {
+    });
+  });
+
+  describe("getTileAtLocation", function() {
+    xit("Returns the appropriate tile at the given location", function() {
+    });
+  });
+
+  describe("switchTurn", function() {
+    xit("Switches player turn", function() {
+    });
+  });
+
+  describe("unitCombat", function() {
+    xit("Combat occurs between the two input units", function() {
+    });
+  });
+});
