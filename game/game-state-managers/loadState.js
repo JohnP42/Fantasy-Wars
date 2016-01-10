405a4293e1b13f05d27353a96133d60c6407f692
ggustilo <beastifer@gmail.com>
2016-01-10 09:35:47 -0600

    Change out map

--- game/game-state-managers/loadState.js
+++ game/game-state-managers/loadState.js
@@ -4,8 +4,8 @@ var loadState = {
 		//TODO: Load assets here
     this.load.image('logo', 'game/assets/logo.jpg');
 
-    game.load.tilemap("testmap", "game/assets/maps/test_map.json", null, Phaser.Tilemap.TILED_JSON );
-    game.load.image("tilesheet", "game/assets/maps/example_tilesheet_2.png");
+    game.load.tilemap("testmap", "game/assets/maps/FW_map_1.json", null, Phaser.Tilemap.TILED_JSON );
+    game.load.image("tilesheet", "game/assets/maps/fantasy_wars_tilesheet.png");
 
     game.load.spritesheet("sprDwarves", "game/assets/spritesheets/dwarf_units.png", 32, 32);
 	},
