9ce3c107b67845c5e8a46722c7635590512cd042
Kevin Mark <kmark1625@gmail.com>
2016-01-10 12:58:32 -0600

    Add working terrain

--- game/battle/map/map.js
+++ game/battle/map/map.js
@@ -9,8 +9,23 @@ Map.prototype.getTileAtPos = function(pos) {
 
 Map.prototype.getPenaltyAtPos = function(pos, unit) {
   // This line is causing problems
-  // var tile = this.getTileAtPos(pos);
-  return 1;
+  var tile = this.getTileAtPos(pos);
+  if (unit instanceof UnitInfantry) {
+    return parseInt(tile.movCostInf);
+  } else if (unit instanceof UnitArtillery) {
+    return parseInt(tile.movCostArt);
+  } else if (unit instanceof UnitFlying) {
+    return parseInt(tile.movCostFly);
+  } else if (unit instanceof UnitCavalry) {
+    return parseInt(tile.movCostCav);
+  } else {
+    return 1;
+  }
+}
+
+Map.prototype.posValid = function(pos) {
+  var map = game.world.cursor.map;
+  return (pos.x >=0 && pos.x < map.width && pos.y >= 0 && pos.y < map.height);
 }
 
 
