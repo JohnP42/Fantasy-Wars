c0878bde81ee6efda0422070198295b8b6c9fd84
Max Iniguez <email": null>
2016-01-11 14:00:21 -0600

    Working audio

--- game/game-state-managers/mainMenuState.js
+++ game/game-state-managers/mainMenuState.js
@@ -2,14 +2,14 @@ var mainMenuState = {
 
 	create: function() {
 		//TODO: anything needed on menu start up add here
-    var bgm = game.add.audio('menus');
-    var logo = this.add.image(0, 0, 'logo');
-    logo.width = this.game.width;
-    logo.height = this.game.height;
+        var bgm = game.add.audio('menus');
+        var logo = this.add.image(0, 0, 'logo');
+        logo.width = this.game.width;
+        logo.height = this.game.height;
 
-    var menuButton = new MenuButton(528, 200, 'battleButton', "mapSelectState", "battleButton");
-    // this.sound.setDecodedCallback(bgm, this.start, this);
-    bgm.play();
+        var menuButton = new MenuButton(528, 200, 'battleButton', "mapSelectState", "battleButton");
+        // this.sound.setDecodedCallback(bgm, this.start, this);
+        bgm.play();
 	},
 
 	update: function() {
