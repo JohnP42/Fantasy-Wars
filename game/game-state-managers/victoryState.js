c34ba2e9ea74987600e231710ab8f78c33d72be6
ggustilo <beastifer@gmail.com>
2016-01-11 19:15:12 -0600

    Add working victory state and additional menus

--- game/game-state-managers/victoryState.js
+++ game/game-state-managers/victoryState.js
@@ -1,10 +1,13 @@
 var victoryState = {
 
-	create: function() {
+    init: function(player) {
+        this.winner = player;
+    },
 
-            var victoryStyle = {font: "bold 48pt Herculanum", align: "center"};
-            var victoryBackGround = game.add.image(0, 0, "parchment");
-            var victoryText = game.add.text(300, 320, "Player" + battle.currentPlayer, victoryStyle);
+	create: function() {
+            var victoryStyle = {font: "bold 48pt Herculanum", align: "center", fill: "red"};
+            var parchment = this.add.image(0, 0, 'parchment');
+            var victoryText = game.add.text(150, 220, "Player " + this.winner + " wins!" , victoryStyle);
 	},
 
 	update: function() {
