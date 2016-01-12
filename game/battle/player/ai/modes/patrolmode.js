07d9349adf0e998c29d86ac6876b86113b1740ec
Kevin Mark <kmark1625@gmail.com>
2016-01-12 09:26:36 -0600

    Add pseudocode for AI modes

--- game/battle/player/ai/modes/patrolmode.js
+++ game/battle/player/ai/modes/patrolmode.js
@@ -2,9 +2,13 @@ PatrolMode.prototype = new Mode();
 PatrolMode.prototype.constructor = PatrolMode;
 
 function PatrolMode() {
-
 };
 
 PatrolMode.prototype.execute = function() {
+  nextMode = "patrol";
   console.log("Implementing execute function");
+  // Move towrds buildings with infantry to capture them
+  // Move towards your infantry units to provide protection
+  // Build units to reinforce the army
+  return nextMode;
 };
