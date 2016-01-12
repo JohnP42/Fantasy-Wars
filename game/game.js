6c6cf7b69983f75c36cce44d046c3020dcbb66f5
Kevin Mark <kmark1625@gmail.com>
2016-01-11 21:02:18 -0600

    Add refactored loadState.js

--- game/game.js
+++ game/game.js
@@ -1,7 +1,13 @@
 window.onload = function() {
-
   game = new Phaser.Game(800, 600, Phaser.AUTO, '');
 
+  _addGameStates();
+
+  game.state.start("bootState");
+};
+
+function _addGameStates() {
+  // Add necessary game states to the Phaser Game State Manager
   game.state.add("bootState", bootState);
   game.state.add("loadState", loadState);
   game.state.add("mainMenuState", mainMenuState);
@@ -9,6 +15,4 @@ window.onload = function() {
   game.state.add("armySelectState", armySelectState);
   game.state.add("battleState", battleState);
   game.state.add("victoryState", victoryState);
-  game.state.start("bootState");
-
 };
