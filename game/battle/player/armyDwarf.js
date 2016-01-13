0bedad82162ab9810f300188cf213c8fcfcef9d3
ggustilo <beastifer@gmail.com>
2016-01-13 10:08:44 -0600

    Fix testing bugs from victory conditions - still need to assign HQ at start of battle - map to hold info

--- game/battle/player/armyDwarf.js
+++ game/battle/player/armyDwarf.js
@@ -2,15 +2,15 @@ ArmyDwarf.prototype = new Army();
 ArmyDwarf.prototype.constructor = ArmyDwarf;
 
 function ArmyDwarf(units) {
-	this.units = units;
-  this.armyList = [
-    Warrior,
-    Grenadier,
-    Mech,
-    MotorBike,
-    IronGuard,
-    Mortar,
-    Cannon,
-    Biplane
-  ];
+    this.units = units;
+    this.armyList = [
+        Warrior,
+        Grenadier,
+        Mech,
+        MotorBike,
+        IronGuard,
+        Mortar,
+        Cannon,
+        Biplane
+    ];
 };
